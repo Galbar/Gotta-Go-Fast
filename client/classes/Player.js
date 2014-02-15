62ec68839f7687500df5df99ea900aeff824d217
Sergi Sorigue <darkdiabl3@gmail.com>
2014-02-15 03:54:29 +0100

    a

--- client/classes/Player.js
+++ client/classes/Player.js
@@ -1,41 +1,35 @@
 //var user = new Image();
 //userIMG.src = "img/user.png";
 
-function Player (x,y,id) {
-	this.x = x;
-	this.y = y;
+function Player (id) {
+	this.x;
+	this.y;
 	this.id = id;
+    this.name;
 	this.width = 50;
 	this.height = 100;
     this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
     this.vx = 0;
     this.vy = 0;
-    this.speed = 10;
+    this.speedx = 10;
+    this.speedy = 3;
 	//this.image = userIMG;
 }
 
 function command (c) {
-    if (c == "U") {
-        this.vy = this.speedy;
-    }
-    else {
-
-    };
+    if (c === "U") this.vy = this.speedy;
+    else if (c === "A") this.vx = -this.speedx;
+    else if (c === "D") this.vx = this.speedx; 
 }
-
 
 Player.prototype.update = function(dt) {
     this.x += this.vx*dt;
     this.y += this.vy*dt;
-    if (vy > 0) {
-        vy += -3*dt;
-    };
-    if (vy < 0) {
-        vy = 0;
-    };
-    if (this.y < 0) {
-        this.y = 0;
-    };
+
+    if (vy > 0) vy += -3*dt;
+    if (vy < 0) vy = 0;
+
+    if (this.y < 0) this.y = 0;
 
     // W
     if (e.keyCode === 87) this.y += -this.speed*dt;
