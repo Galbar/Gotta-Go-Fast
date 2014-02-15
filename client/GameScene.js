850533cf0a35839582135b4c143560a62bc6aa8a
Sergi Sorigue <darkdiabl3@gmail.com>
2014-02-15 02:37:13 +0100

    Player OP

--- client/GameScene.js
+++ client/GameScene.js
@@ -1,24 +1,8 @@
-function Slot () {
-    // Asigno una altura random
-    this.x = Math.random()*(canvas.width-2*this.w)+this.w;
-    this.y = 100;
-    this.posx = canvas.height;
-    this.posy = canvas.width;
-}
-
 function update (dt) {
-
+    for (var obj in players) players[obj].update(dt);
 }
 
 function draw (ctx) {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    // Poso 8 perque hi hagi 8 slots posibles en pantalla
-    for (var i = 0; i < 8; ++i) {
-
-    }
-
-
-    for (var obj in players) players[obj].render(ctx);
-
-
+    for (var obj in players) players[obj].draw(ctx);
+    for (var obj in obstacles) obstacles[obj].render(ctx);
 }
--- client/classes/Player.js
+++ client/classes/Player.js
@@ -1,5 +1,5 @@
-var user = new Image();
-userIMG.src = "img/user.png";
+//var user = new Image();
+//userIMG.src = "img/user.png";
 
 function Player (x,y,id) {
 	this.x = x;
@@ -7,17 +7,29 @@ function Player (x,y,id) {
 	this.id = id;
 	this.width = 50;
 	this.height = 100;
-	this.image = userIMG;
+    this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 10;
+	//this.image = userIMG;
 }
 
-Player.prototype.render = function(ctx) {
-	ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-
+Player.prototype.update = function(dt) {
+    // W
+    if (e.keyCode === 87) this.y += -speed*dt;
+    // A
+    if (e.keyCode === 65) this.x += -speed*dt;
+    // S
+    if (e.keyCode === 83) this.y += speed*dt;
+    // D
+    if (e.keyCode === 68) this.x += speed*dt;
 }
 
-Player.prototype.updateWithPlayerData = function (data) {
-	if (data === undefined) return;
-	if (data.id !== undefined) this.id = data.id;
-	if (data.x !== undefined) this.x = data.x;
-	if (data.y !== undefined) this.y = data.y;
+Player.prototype.draw = function(ctx) {
+	ctx.fillStyle = this.color;
+	ctx.fillRect(this.x, this.y, this.width, this.height);
 }
+
+document.addEventListener("keydown", function(e) {
+    console.log(e.keyCode)
+});
