9df32a1ebdd99d4df4a97577a2e23aa9c96c652e
Obscurs <ferris202@gmail.com>
2014-02-15 19:05:56 +0100

    cancer de sida

--- client/classes/Obstacle.js
+++ client/classes/Obstacle.js
@@ -6,12 +6,16 @@ function Obstacle () {
     this.height = Math.random()*(canvas.width-2*this.w)+this.w;
     this.color = "red";
     this.speed = 3;
+    this.sizeup = 0;
+    this.sizedown = 0;
 }
 
+Obstacle.prototype.generar = function(prev){
+    
+}
 Obstacle.prototype.update = function(dt) {
     this.x += -this.speed*dt;
 }
-
 Obstacle.prototype.draw = function(ctx) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x,this.y,this.width,this.height);
